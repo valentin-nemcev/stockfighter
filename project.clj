4aa217f3fa78dc63867d16b19d581d50e358dd18
Валентин Немцев <mail@valentin-nemcev.com>
2016-01-21 21:25:27 +0300

    Read api key from secrets config file

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [
+                 [org.clojure/clojure "1.8.0"]
+                 [clj-http "2.0.1"]
+                 [sonian/carica "1.2.1"]
+                 ]
   :main ^:skip-aot stockfighter.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/stockfighter/core.clj
+++ src/stockfighter/core.clj
@@ -1,7 +1,14 @@
 (ns stockfighter.core
-  (:gen-class))
+  (:gen-class)
+  (:require [carica.core :refer [config configurer resources]])
+  )
+
+(def secrets-config (configurer (resources "secrets-config.edn")))
+
+(def api-key (secrets-config :api-key))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Api key is" api-key))
